fb0cb99a6b144e58b4aad81154945b40bcb51b4c
John Rampelt <johnrampelt@gmail.com>
2023-05-09 12:30:25 -0400

    Update Program Analysis Activities

--- UBMP420-Starter-1-TV-Remote.X/TV-Remote.c
+++ UBMP420-Starter-1-TV-Remote.X/TV-Remote.c
@@ -1,12 +1,12 @@
 /*==============================================================================
  Project: TV-Remote                     Activity: mirobo.tech/ubmp4-starter-1
- Date:    Feb 13, 2023
+ Date:    May 9, 2023
  
- This starter programming project for the mirobo.tech UBMP4 demonstrates how
- to modulate an LED to create SONY-formatted (SIRC) IR remote control codes.
- Using the concepts introduced in this program and following the website
- activity and debugging techniques, you should be able to easily modify this
- starter program to create IR transmissions modulated using other IR protocols.
+ This starter programming project for the mirobo.tech UBMP4 modulates an IR LED
+ to create SONY-formatted (SIRC) remote control codes. By using the concepts
+ introduced in this program and by following the explanations and debugging
+ techniques from the website, you should be able to easily modify this starter
+ program to create IR transmissions modulated using other control protocols.
 ==============================================================================*/
 
 #include    "xc.h"              // Microchip XC8 compiler include file
@@ -42,7 +42,7 @@
 // Sony IR device (address) codes
 #define SONY_TV     0x01        // TV device code
 
-// Sony IR protocol definitions
+// Sony IR protocol modulation details
 #define SONY_START_PULSES   96  // Number of 38kHz IR pulses in a start bit
 #define SONY_1_PULSES       48  // Number of IR pulses to encode a one bit
 #define SONY_0_PULSES       24  // Number of IR pulses to encode a zero bit
@@ -51,7 +51,7 @@
 #define SONY_DEVICE_BITS    5   // Number of device (address) bits to be sent
 
 
-void ir_pulse_40k(unsigned int pulses) // Makes requested number of 40kHz pulses
+void ir_pulse_40k(unsigned int pulses) // Make requested number of 40kHz pulses
 {
     for (pulses; pulses != 0; pulses--) {
         LED6 = 1;               // Create 25us period waves
@@ -118,28 +118,28 @@ int main(void)
 
 /* Learn More - Program Analysis Activities
  * 
- * 1. Not all IR protocols encode data using a 40kHz carrier. The NEC protocol
- *    uses a 38kHz carrier, and the Philips RC-6 protocol uses a 36kHz carrier.
- *    Create and debug a pulse function to produce modulated pulses at one of
- *    these other frequencies.
+ * 1.   Not all IR protocols encode data using a 40kHz carrier. The NEC protocol
+ *      uses a 38kHz carrier, and the Philips RC-6 protocol uses a 36kHz
+ *      carrier. Create and debug a pulse function to produce any number of
+ *      modulated pulses at one of these other commonly-used frequencies.
  * 
- * 2. The NEC protocol (or a slight modification of it) is used by many brands
- *    of televisions including LG and Samsung. Research the NEC protocol. What
- *    type of modulation is used to encode its data?
+ * 2.   The NEC protocol (or a slight modification of it) is used by many brands
+ *      of televisions including LG and Samsung. Research the NEC protocol. What
+ *      type of modulation is used to encode its data?
  * 
- * 3. Determine the number of pulses required to create a start bit, a zero bit,
- *    and a one bit for the NEC protocol using a 38kHz carrier. 
+ * 3.   Determine the number of pulses required to create a start bit, a zero
+ *      bit, and a one bit for the NEC protocol when using a 38kHz carrier.
  * 
- * 4. You can clip an oscilloscope lead to the output pin of the UBMP4 IR
- *    demodulator (U2) to view remote control pulses from many remote control
- *    transmitters without needing to write any software code. See if you can
- *    decode and reverse engineer the IR protocol transmitted by a remote
- *    control transmitter that you own or have access to.
+ * 4.   You can clip an oscilloscope lead to the output pin of UBMP4's IR
+ *      demodulator (U2) to view the remote control pulses from many type of
+ *      remote control transmitters without needing to write any software code.
+ *      See if you can decode and reverse engineer the IR protocol transmitted
+ *      by a remote control transmitter that you own or have access to.
  * 
- * 5. Create a flowchart of the software functions that would be needed to
- *    transmit a protocol different from the SIRC protocol.
+ * 5.   Create a flowchart of the software functions that would be needed to
+ *      transmit a protocol different from the SIRC protocol.
  *
- * 6. IR protocols can be created to implement your own controllable devices.
- *    Think about the types of commands you would need to encode to remotely
- *    control a robot, or a string of NeoPixel LEDs.
+ * 6.   IR protocols can be created to control your own devices. Think about
+ *      the types of commands you would need to encode to remotely control a
+ *      robot, or a string of NeoPixel LEDs.
  */
